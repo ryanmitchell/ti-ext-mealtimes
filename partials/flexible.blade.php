2d29189df122a4466cf2e9f196e6bcdabbede69e
Ryan Mitchell <ryan@thoughtcollective.com>
2022-02-28 19:37:19 +0000

    Switch to blade for flexible include

--- partials/flexible.blade.php
+++ partials/flexible.blade.php
@@ -1,6 +1,8 @@
-<?php $fieldOptions = $field->value;
-$weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-?>
+@php
+    $fieldOptions = $field->value;
+    $weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    $index = 0;
+@endphp
 <div class="field-flexible-hours">
     <div class="row">
         <div class="col-sm-7">
@@ -9,34 +11,33 @@ $weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
                     <thead>
                     <tr>
                         <th></th>
-                        <th><?= lang('thoughtco.mealtimes::default.start_time');?></th>
-                        <th><?= lang('thoughtco.mealtimes::default.end_time');?></th>
-                        <th><?= lang('thoughtco.mealtimes::default.available');?></th>
+                        <th>@lang('thoughtco.mealtimes::default.start_time')</th>
+                        <th>@lang('thoughtco.mealtimes::default.end_time')</th>
+                        <th>@lang('thoughtco.mealtimes::default.available')></th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php $index = 0;
-                    foreach ($weekdays as $key => $day) { ?>
-                        <?php
+                    @foreach ($weekdays as $key => $day)
+                        @php
                         $hour = (isset($fieldOptions[$key])) ? $fieldOptions[$key] : ['day' => $key, 'open' => '00:00', 'close' => '23:59', 'status' => 1]
-                        ?>
+                        @endphp
                         <tr>
                             <td>
-                                <span><?= $day; ?></span>
+                                <span>{{ $day }}</span>
                                 <input
                                     type="hidden"
-                                    name="<?= $field->getName(); ?>[<?= $index; ?>][day]"
-                                    value="<?= $hour['day']; ?>"/>
+                                    name="{{ $field->getName() }}[{{ $index }}][day]"
+                                    value="{{ $hour['day'] }}"/>
                             </td>
                             <td>
                                 <div class="input-group" data-control="clockpicker" data-autoclose="true">
                                     <input
                                         type="text"
-                                        name="<?= $field->getName() ?>[<?= $index; ?>][open]"
+                                        name="{{ $field->getName() }}[{{ $index }}][open]"
                                         class="form-control"
                                         autocomplete="off"
-                                        value="<?= $hour['open'] ?>"
-                                        <?= $field->getAttributes() ?> />
+                                        value="{{ $hour['open'] }}"
+                                        {{ $field->getAttributes() }}
                                     <span class="input-group-prepend">
                                 <span class="input-group-icon"><i class="fa fa-clock-o"></i></span>
                             </span>
@@ -46,11 +47,11 @@ $weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
                                 <div class="input-group" data-control="clockpicker" data-autoclose="true">
                                     <input
                                         type="text"
-                                        name="<?= $field->getName() ?>[<?= $index; ?>][close]"
+                                        name="{{ $field->getName() }}[{{ $index }}][close]"
                                         class="form-control"
                                         autocomplete="off"
-                                        value="<?= $hour['close'] ?>"
-                                        <?= $field->getAttributes() ?> />
+                                        value="{{ $hour['close'] }}"
+                                        {{ $field->getAttributes() }}
                                     <span class="input-group-prepend">
                                 <span class="input-group-icon"><i class="fa fa-clock-o"></i></span>
                             </span>
@@ -65,32 +66,32 @@ $weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 	                                	<div class="custom-control custom-switch">
 		                                    <input
 		                                        type="checkbox"
-		                                        name="<?= $field->getName() ?>[<?= $index; ?>][status]"
-		                                        id="<?= $field->getId($index.'status') ?>"
+                                                name="{{ $field->getName() }}[{{ $index }}][status]"
+		                                        id="{{ $field->getId($index.'status') }}"
 		                                        class="custom-control-input"
 		                                        value="1"
-		                                        <?= $this->previewMode ? 'disabled="disabled"' : '' ?>
-		                                        <?= isset($hour['status']) && $hour['status'] == 1 ? 'checked="checked"' : '' ?>
-		                                        <?= $field->getAttributes() ?>
+		                                        {!! $this->previewMode ? 'disabled="disabled"' : '' !!}
+		                                        {!! isset($hour['status']) && $hour['status'] == 1 ? 'checked="checked"' : '' !!}
+		                                        {!! $field->getAttributes() !!}
 		                                    >
 		                                    <label
 		                                        class="custom-control-label"
-		                                        for="<?= $field->getId($index.'status') ?>"
+		                                        for="{{ $field->getId($index.'status') }}"
 		                                    >
-		                                    	<?= lang('thoughtco.mealtimes::default.available_yes_no');?>
-		                                    </label>                                	
+		                                    	@lang('thoughtco.mealtimes::default.available_yes_no')
+		                                    </label>
 	                                	</div>
 	                                </div>
                                 </div>
                             </td>
                         </tr>
-                        <?php
+                        @php
                         $index++;
-                        ?>
-                    <?php } ?>
+                        @endphp
+                    @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-</div>	
+</div>
